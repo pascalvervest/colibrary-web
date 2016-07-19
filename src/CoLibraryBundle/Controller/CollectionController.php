873904fcba6cbc1ac980ebd5bcae85c542cf08c3
Pascal Vervest <pascalvervest@gmail.com>
2016-07-20 00:08:22 +0200

    Edit items in collection

--- src/CoLibraryBundle/Controller/CollectionController.php
+++ src/CoLibraryBundle/Controller/CollectionController.php
@@ -9,6 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Class CollectionController
@@ -63,6 +64,40 @@ class CollectionController extends Controller
         
         return [
             'form' => $form->createView()
+        ];
+    }
+
+    /**
+     * @Route("collection/edit/{id}", name="collection_edit")
+     * @Template
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function editAction(Request $request)
+    {
+        $item = $this->getDoctrine()
+            ->getRepository(Item::class)
+            ->findOneById($request->get('id'));
+
+        if (!$item) {
+            throw new NotFoundHttpException('Item not found');
+        }
+
+        $form = $this->createForm(ItemType::class, $item);
+        $form->handleRequest($request);
+
+        // Validate form and save to database
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($item);
+            $em->flush();
+
+            return $this->redirectToRoute('collection_index');
+        }
+
+        return [
+            'form' => $form->createView(),
         ];
     }
 }
